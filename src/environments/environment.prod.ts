dbdb2a1cdd420e3a51d230cbbdcafdeec10b7898
Daniel <dani+github@daigeb.ch>
2019-10-17 00:05:37 +0200

    Prod config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,11 +4,10 @@ export const environment = {
     CLIENT_ID: 'NBBkdTDcCp2MW07SHvU743q4W505iFtR',
     CLIENT_DOMAIN: 'rvw.eu.auth0.com', // e.g., 'you.auth0.com'
     AUDIENCE: 'https://api.aws.daigeb.ch',
-    REDIRECT: 'http://localhost:4200/callback',
-    LOGOUT_URL: 'http://localhost:4200'
+    REDIRECT: 'https://rvw.cycled.ch/callback',
+    LOGOUT_URL: 'https://rvw.cycled.ch'
   },
   api: {
-    roles: '',
     url: 'https://api.aws.daigeb.ch'
   }
 };
